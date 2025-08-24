6fa8fa3aa14c059ced58315da837d50ed44b7779
Christian Arroyo <ChristianArroyo308@yahoo.com>
2025-08-24 13:11:22 -0700

    implemented increment and decrement functions for copies of cards

--- src/components/HomePageComponent.jsx
+++ src/components/HomePageComponent.jsx
@@ -7,7 +7,27 @@ export default function HomePageComponent() {
 
   const [cards, setCards] = useState(cardsImport);
 
+   const incrementCopies = (cardNumber) => {
+    setCards((prev) => {
+      const newCards = [...prev];
+      newCards[cardNumber - 1] = {
+        ...newCards[cardNumber - 1], 
+        copies: Math.min(9, newCards[cardNumber - 1].copies + 1), 
+      };
+      return newCards;
+    });
+  };
 
+   const decrementCopies = (cardNumber) => {
+    setCards((prev) => {
+      const newCards = [...prev];
+      newCards[cardNumber - 1] = {
+        ...newCards[cardNumber - 1], 
+        copies: Math.max(0, newCards[cardNumber - 1].copies - 1), 
+      };
+      return newCards;
+    });
+  };
 
   return (
     <>
@@ -17,18 +37,6 @@ export default function HomePageComponent() {
               <h1 className='text-2xl font-bold text-blue-700 md:hidden'>Pocket Trading</h1>
               <h2 className='font-extralight text-gray-600 text-base'>No account needed</h2>
             </div>
-            {/* <section className='relative w-full h-auto'>
-              <img className='w-full h-auto' src="images/pexels1.jpg" alt="Picture of a classroom" />
-              <div className='absolute inset-0 w-7/10 bg-gradient-to-r from-gray-900/90 to-gray-500/5'></div>
-              <div className='absolute h-full flex flex-col top-0 justify-center left-[7%]'>
-                <h3 className='font-bold text-white text-2xl sm:text-4xl md:text-5xl lg:text-6xl'>A place to learn,</h3>
-                <h3 className='font-bold text-white text-2xl sm:text-4xl md:text-5xl lg:text-6xl'>play, and grow.</h3>
-                <h3 className='mt-3 hidden font-extralight text-neutral-200 text-base sm:block md:text-lg lg:text-2xl'>Dedicated to offering a loving,</h3>
-                <h3 className='hidden font-extralight text-neutral-200 text-base sm:block md:text-lg lg:text-2xl'>educational, and fun experience.</h3>
-                <Link to='/contact' className='px-2 py-1 mt-4 w-fit text-base font-bold bg-gradient-to-r from-blue-700 to-blue-500 text-white hover:shadow-xl hover:text-gray-200 shadow-sm shadow-neutral-600 rounded-lg sm:px-4 sm:py-2 sm:text-xl md:text-2xl sm:mt-6' href="">Call To Action</Link>
-              </div>
-            </section> */}
-
             <section>
               <div className='flex flex-wrap gap-x-16 gap-y-12'>
                 {cards.map((card) => (
@@ -36,12 +44,11 @@ export default function HomePageComponent() {
                     <div className='h-[11rem] w-[8rem] border-black border'></div>
                     <p className='text-sm'>#{card.index}</p>
                     <p className='text-xl'>{card.name}</p>
-                    {/* <p>#{card.index}</p> */}
                     <p className='text-sm'>{card.rarity.value} {card.rarity.type}</p>
                     <div className='flex gap-6 text-xl'>
-                      <p className='px-2 rounded-full border-black border'>-</p>
+                      <p className='px-2 rounded-full border-black border' onClick={() => decrementCopies(card.index)}>-</p>
                       <p className=''>{card.copies}</p>
-                      <p className='px-2 rounded-full border-black border'>+</p>
+                      <p className='px-2 rounded-full border-black border' onClick={() => incrementCopies(card.index)}>+</p>
                     </div>
                   </div>
                 ))}
