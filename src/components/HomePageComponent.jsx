b8a6fd40499818098acf5931cb95e035cea2a92c
Christian Arroyo <ChristianArroyo308@yahoo.com>
2025-09-01 11:52:24 -0700

    making button copy to clipboard. Filters w-fit

--- src/components/HomePageComponent.jsx
+++ src/components/HomePageComponent.jsx
@@ -5,9 +5,16 @@ import { useState } from 'react'
 
 export default function HomePageComponent({cards, setCards}) {
 
-  const handleClickURL = () => {
-    const url = cards.reduce((acc, card) => acc + card.copies.toString(), "")
-    console.log(url)
+  const textToCopy = cards.reduce((acc, card) => acc + card.copies.toString(), "")
+
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(textToCopy);
+      alert("Copied to clipboard");
+    } catch (err) {
+      console.error("Failed to copy: ", err);
+      alert("Failed to copy");
+    }
   }
 
   const [filterOut, setFilterOut] = useState(
@@ -54,18 +61,18 @@ export default function HomePageComponent({cards, setCards}) {
   return (
     <>
       <div className='mt-2 flex flex-col gap-8 px-[6%]'>
-          <main className='flex flex-col gap-6 justify-center md:gap-16'>
+          <main className='flex flex-col gap-6 justify-center md:gap-10'>
             <div className='mt-2 text-center md:hidden'>
               <h1 className='text-2xl font-bold text-blue-700 md:hidden'>Pocket Trading</h1>
               <h2 className='font-extralight text-gray-600 text-base'>No account needed</h2>
             </div>
             <div>
-              <button className='px-4 py-2 rounded-lg bg-blue-500 text-white' onClick={() => handleClickURL()}>Create URL</button>
+              <button className='px-4 py-2 rounded-lg bg-blue-500 text-white' onClick={() => handleCopy()}>Copy URL</button>
             </div>
             <div className='flex flex-col gap-1'>
               <p>Filters</p>
               {Object.keys(filterOut).map((key) => (
-                <label key={key}>
+                <label key={key} className='px-2 py-1 w-fit'>
                   <input className='mx-2'
                     type="checkbox"
                     checked={!filterOut[key]}
